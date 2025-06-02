1ac49910fa6c563f27a2bda7ab049989ecb28826
QuentinW59 <quentin.wisniewski@outlook.fr>
2025-06-02 20:00:17 +0200

    ajout du changement de background et résolution problème partage de co et expo

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import { ImageBackground, Text, View } from "react-native";
 import { s } from "./App.style";
 import hotBackground from "./assets/hot.png";
+import coldBackground from "./assets/cold.png";
 import { InputTemperature } from "./inputs/inputTemperature";
 import { TemperatureDisplay } from "./inputs/temperatureDisplay";
 import { DEFAULT_TEMPERATURE, DEFAULT_UNIT, UNITS } from "./constant";
-import { use, useState } from "react";
-import { getOppositeUnit, convertTemperatureTo } from "./services/temperature.service";
+import { use, useEffect, useState } from "react";
+import { getOppositeUnit, convertTemperatureTo, IsIceTemperature} from "./services/temperature.service";
 import { ButtonConvert } from "./buttons/buttonConvert";
 
 export default function App(){
   const [inputValue, setInputValue] = useState(DEFAULT_TEMPERATURE);
   const [currentUnit, setCurrentUnit] = useState(DEFAULT_UNIT);
+  const [currentBackground, setCurrentBackground] = useState();
   const oppositeUnit = getOppositeUnit(currentUnit);
+
+  useEffect(() => {
+    const temperatureAsFloat = Number.parseFloat(inputValue);
+    if(!isNaN(temperatureAsFloat)) {
+      const isColdBackground = IsIceTemperature(inputValue, currentUnit);
+      setCurrentBackground(isColdBackground ? coldBackground : hotBackground);
+    }
+  }, [inputValue, currentUnit]);
 
   function getConvertedTemperature() {
     const valueAsFloat = Number.parseFloat(inputValue);
@@ -21,7 +31,7 @@ export default function App(){
   }
 
   return (
-    <ImageBackground source={hotBackground} style={s.container}>
+    <ImageBackground source={currentBackground} style={s.container}>
       <View style={s.workspace}>
         <TemperatureDisplay value={getConvertedTemperature()} unit={oppositeUnit} />
         <InputTemperature 
--- services/temperature.service.js
+++ services/temperature.service.js
@@ -5,11 +5,19 @@ function getOppositeUnit(unit) {
 }
 
 function convertTemperatureTo(unit, value) {
-    if (unit ===UNITS.celsius){
+    if (unit === UNITS.celsius){
         return (value -32) / 1.8;
     } else {
         return value * 1.8 + 32;
     }
 }
 
-export {getOppositeUnit, convertTemperatureTo};
+function IsIceTemperature (value, unit){
+    if (unit === UNITS.celsius){
+        return value <= 0;
+    } else {
+        return value <= 32;
+    }
+}
+
+export {getOppositeUnit, convertTemperatureTo, IsIceTemperature};
