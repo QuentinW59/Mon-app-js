3d5bc7784954f51b57b7b805458c5d3280c5a88e
QuentinW59 <quentin.wisniewski@outlook.fr>
2025-06-02 16:05:04 +0200

    mise en place des premiers composants input et display temperature

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, Text, View } from "react-native";
+import { s } from "./App.style";
+import hotBackground from "./assets/hot.png";
+import { InputTemperature } from "./inputs/inputTemperature";
+import { TemperatureDisplay } from "./inputs/temperatureDisplay";
+import { DEFAULT_TEMPERATURE, DEFAULT_UNIT, UNITS } from "./constant";
+import { use, useState } from "react";
 
-export default function App() {
+export default function App(){
+  const [inputValue, setInputValue] = useState(DEFAULT_TEMPERATURE);
+  const [currentUnit, setCurrentUnit] = useState(DEFAULT_UNIT);
+
+
   return (
-    <View style={styles.container}>
-      <Text>il fonctionne ton docker ? </Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={hotBackground} style={s.container}>
+      <View style={s.workspace}>
+        <TemperatureDisplay value={inputValue} unit={currentUnit} />
+        <InputTemperature 
+          onChangeText={setInputValue}
+          defaultValue={DEFAULT_TEMPERATURE}
+        />
+        <View>
+          <Text>Button</Text>
+        </View>
+      </View>
+    </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ constant.js
@@ -0,0 +1,7 @@
+export const DEFAULT_TEMPERATURE = "0";
+export const UNITS = {
+    celsius: "°C",
+    faranheit: "°F",
+};
+
+export const DEFAULT_UNIT= UNITS.celsius;
