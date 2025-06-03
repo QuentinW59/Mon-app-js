e37e128d267c1d8666bc1a31806a06a4ff2bde20
QuentinW59 <quentin.wisniewski@outlook.fr>
2025-06-03 10:29:49 +0200

    fix : erreur unit dynamic

--- App.js
+++ App.js
@@ -37,6 +37,7 @@ export default function App(){
         <InputTemperature 
           onChangeText={setInputValue}
           defaultValue={DEFAULT_TEMPERATURE}
+          unit = {currentUnit}
         />
         <ButtonConvert 
         onPress={() => {setCurrentUnit(oppositeUnit);
